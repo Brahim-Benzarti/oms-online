4705be3f372affdb0128b485f5cfed0f805c6a84
Brahim Benzarti <benzartibrahim54@gmail.com>
2021-07-17 13:39:00 +0100

    changing database

--- dbcon.php
+++ dbcon.php
@@ -1,9 +1,9 @@
 <?php
 
-$servername="freedb.tech";
-$username="freedbtech_omsadminproject";
-$password="brahimtheadmin";
-$dbname="freedbtech_oms";
+$servername="sql5.freesqldatabase.com";
+$username="sql5426001";
+$password="BfXiJhKS9a";
+$dbname="sql5426001";
 
 $conn=mysqli_connect($servername, $username, $password, $dbname);
 
