23fc2a05986922cd23c39cafd578d11ab0850939
Brahim Benzarti <benzartibrahim54@gmail.com>
2021-07-17 15:31:26 +0100

    changing database

--- dbcon.php
+++ dbcon.php
@@ -11,7 +11,7 @@ $conn=mysqli_connect($servername, $username, $password, $dbname);
 if(!$conn){
     echo("<script>alert('creating data base');</script>");
     $pre_conn=mysqli_connect($servername, $username, $password);
-    mysqli_query($pre_conn,"CREATE Database oms;")or die("wtf");
+    mysqli_query($pre_conn,"CREATE Database '$dbname';")or die("wtf");
     $conn=mysqli_connect($servername, $username, $password, $dbname);
     if(mysqli_multi_query($conn,file_get_contents("../oms.sql"))){
         do {
@@ -25,6 +25,25 @@ if(!$conn){
             };
              //Prepare next result set
           } while (mysqli_next_result($conn));
+    }
+}else{
+    $req="select * from users;";
+    $res=mysqli_query($conn,$req);
+    if(!$res){
+        $conn=mysqli_connect($servername, $username, $password, $dbname);
+        if(mysqli_multi_query($conn,file_get_contents("../oms.sql"))){
+            do {
+                // Store first result set
+                if ($result = mysqli_store_result($conn)) {
+                    $result -> free_result();
+                };
+                // if there are more result-sets, the print a divider
+                if (mysqli_more_results($conn)) {
+                    continue;
+                };
+                //Prepare next result set
+            } while (mysqli_next_result($conn));
+        }
     }
 }
 ?>
