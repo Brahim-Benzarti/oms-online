957c1934fea07876f92652005574624a68c1dcc9
Brahim Benzarti <benzartibrahim54@gmail.com>
2021-07-17 13:47:03 +0100

    changing database

--- dbcon.php
+++ dbcon.php
@@ -6,11 +6,13 @@ $password="BfXiJhKS9a";
 $dbname="sql5426001";
 
 $conn=mysqli_connect($servername, $username, $password, $dbname);
+
 //creating database if doesn't exits.. 
 if(!$conn){
     $pre_conn=mysqli_connect($servername, $username, $password);
     mysqli_query($pre_conn,"CREATE Database oms;")or die("wtf");
     $conn=mysqli_connect($servername, $username, $password, $dbname);
+    if(!$conn){die("shit")};
     if(mysqli_multi_query($conn,file_get_contents("../oms.sql"))){
         do {
             // Store first result set
